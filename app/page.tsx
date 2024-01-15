4c52784e2027cc575f8f8a02652d228e817c5d7b
Ubirajara M Neto <ubirajaramneto@gmail.com>
2024-01-15 18:13:43 -0300

    refactor: use new ListingGrid component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Listing } from "@/app/_listing/Listing";
+import { ListingGrid } from "@/app/_listing/ListingGrid";
 import { ListingType, ListingData } from "@/app/lib/listing/data";
 
 export default async function Home() {
@@ -6,7 +6,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Listing listing={listing} />
+      <ListingGrid listing={listing} />
     </main>
   );
 }
